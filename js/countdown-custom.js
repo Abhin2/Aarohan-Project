1612952b3ce6b59f3030894868935b1c04201725
Abhishek <106398472+Abhin2@users.noreply.github.com>
2024-08-27 20:13:42 +0530

    Add files via upload

---
+++ js/countdown-custom.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+    $(function() {
+        $('#defaultCountdown').countdown({
+            until: new Date(2022, 4, 20, 8)
+        }); // year, month, date, hour
+    });
+});
